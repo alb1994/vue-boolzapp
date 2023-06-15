7a3342f361631c942ed28d501f8ce74146d36d04
alb1994 <antonetti_alberto@libero.it>
2023-06-15 12:28:27 +0200

    aggiornamento esercizio

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            //creo contenitore di messaggi inviati 
+            sentMessages: [] , 
+            //creo contenitore di messaggi ricevuti 
+            receivedMessages: [] ,
+            //contenitore nome dell' oggetto selezionato 
+            contactName: '',
+            //array di oggetti
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    avatar: './img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -29,7 +36,7 @@ createApp({
                 },
                 {
                     name: 'Fabio',
-                    avatar: './img/avatar_2.png',
+                    avatar: './img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -51,7 +58,7 @@ createApp({
                 },
                 {
                     name: 'Samuele',
-                    avatar: './img/avatar_3.png',
+                    avatar: './img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -73,7 +80,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro B.',
-                    avatar: './img/avatar_4.png',
+                    avatar: './img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -90,7 +97,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -107,7 +114,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
@@ -129,7 +136,7 @@ createApp({
                 },
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.png',
+                    avatar: './img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
@@ -146,7 +153,7 @@ createApp({
                 },
                 {
                     name: 'Davide',
-                    avatar: './img/avatar_8.png',
+                    avatar: './img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
@@ -168,10 +175,84 @@ createApp({
                 }
             ]
             
+            
         }
          
     },
-    methods:{
-       
-}
+    
+        methods: {
+            //creo funzione per dividere messaggi inviati e ricevuti 
+            formatMessages(messages) {
+                // Array per i messaggi ordinati 
+                const formattedMessages = []; 
+                // Stato precedente (inizialmente null)
+                let prevStatus = null; 
+              
+                for (let i = 0; i < messages.length; i++) { // Ciclo "for" che scorre gli elementi dell'array "messages"
+                  const message = messages[i]; // Messaggio corrente
+                  const { status } = message; // Estrapola lo stato dal messaggio corrente
+              
+                  if (status !== prevStatus) { // Se lo stato è diverso dallo stato precedente
+                    prevStatus = status; // Aggiorna lo stato precedente con lo stato corrente
+                    formattedMessages.push({ // Aggiungi un nuovo oggetto al formato dei messaggi
+                      ...message, // Copia tutti i campi del messaggio corrente
+                      class: status === 'sent' ? 'inviato' : 'ricevuto' // Aggiunge una proprietà "class" basata sullo stato
+                    });
+                  } else { // Se lo stato è lo stesso dello stato precedente
+                    const lastMessage = formattedMessages[formattedMessages.length - 1]; // Ottieni l'ultimo messaggio formattato
+                    lastMessage.message += '\n' + message.message; // Aggiungi il messaggio corrente al messaggio precedente con un newline
+                  }
+                }
+              
+                return formattedMessages; // Restituisci l'array dei messaggi ordinati
+              },
+              
+              selectContact(index) {
+                // Ottieni il contatto selezionato
+                const selectedContact = this.contacts[index];
+                
+                // Aggiorna le informazioni del contatto attivo
+                this.contactName = selectedContact.name;
+                this.selectedAvatar = selectedContact.avatar;
+              
+                // ordina i messaggi del contatto selezionato
+                const messages = selectedContact.messages.map(message => ({
+                  ...message,
+                  class: message.status === 'sent' ? 'inviato' : 'ricevuto'
+                }));
+              
+                // Formatta i messaggi raggruppandoli per inviati e ricevuti
+                const formattedMessages = this.formatMessages(messages);
+                const sentMessages = formattedMessages.filter(message => message.status === 'sent');
+                const receivedMessages = formattedMessages.filter(message => message.status === 'received');
+              
+                // Aggiorna le variabili di stato dei messaggi inviati e ricevuti
+                this.sentMessages = sentMessages;
+                this.receivedMessages = receivedMessages;
+              
+                // Seleziona l'elemento del chat nel DOM
+                const chatElement = document.querySelector('.chat');
+              
+                // Rimuovi i messaggi precedenti dal chat
+                chatElement.innerHTML = '';
+              
+                // Funzione per creare un elemento messaggio nel DOM
+                const createMessageElement = (message, isSent) => {
+                  const messageDiv = document.createElement('div');
+                  messageDiv.classList.add('messaggio', isSent ? 'inviato' : 'ricevuto');
+                  const messageContent = document.createElement('p');
+                  messageContent.textContent = message.message;
+                  messageDiv.appendChild(messageContent);
+                  return messageDiv;
+                };
+              
+                // Aggiungi i messaggi formattati al chat nel DOM
+                formattedMessages.forEach(message => {
+                  const isSent = message.status === 'sent';
+                  const messageDiv = createMessageElement(message, isSent);
+                  chatElement.appendChild(messageDiv);
+                });
+            }
+    }
+
 }).mount('#app');
